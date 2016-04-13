534818bb988c86ab111eb7df9c81072a7fff4fb4
Chris Richardson <cjr@hotmail.co.uk>
2016-04-13 21:51:18 +0100

    postgres - implement common sql functions

--- postgres/index.js
+++ postgres/index.js
@@ -3,38 +3,83 @@ define(module, (exports, require, make) => {
   var named_param_re = /\:[-a-zA-Z0-9_]+/g;
 
   make({
-  
+
     ns: 'qp-library/postgres',
-    
+
     connection: null,
-    
+
     init: function(o) {
       this.connection = o.connection;
     },
-    
-    select: function(sql, params, done) { },
-    
-    select_all: function(sql, params, done) { },
-    
-    insert: function(sql, params, done) { },
-    
-    update: function(sql, params, done) { },
-    
-    delete: function(sql, params, done) { },
-    
-    execute: function(sql, params, done) { },
-    
-    prepare_sql: function(config) {
-      var sql = config.text;
-      var values = config.values || [];
-      var params = config.params || [];
-      sql = sql.replace(named_param_re, function(match) {
+
+    select: function(cmd) {
+      this.connection.query(this.prepare(cmd), (error, result) => {
+        if (error) {
+          cmd.done(error);
+        } else if (result.rows > 1) {
+          cmd.done(new Error('Select cannot return multiple rows'));
+        } else {
+          cmd.done(null, result.rows[0]);
+        }
+      });
+    },
+
+    select_all: function(cmd) {
+      this.connection.query(this.prepare(cmd), (error, result) => {
+        if (error) {
+          cmd.done(error);
+        } else {
+          cmd.done(null, result.rows);
+        }
+      });
+    },
+
+    insert: function(cmd) {
+      this.connection.query(this.prepare(cmd), (error, result) => {
+        if (error) {
+          cmd.done(error);
+        } else {
+          cmd.done(null, result.rows[0]._id);
+        }
+      });
+    },
+
+    update: function(cmd) {
+      this.connection.query(this.prepare(cmd), (error, result) => {
+        if (error) {
+          cmd.done(error);
+        } else {
+          cmd.done(null, result.rowCount);
+        }
+      });
+    },
+
+    delete: function(cmd) {
+      this.connection.query(this.prepare(cmd), (error, result) => {
+        if (error) {
+          cmd.done(error);
+        } else {
+          cmd.done(null, result.rowCount);
+        }
+      });
+    },
+
+    execute: function(cmd) {
+      this.connection.query(this.prepare(cmd), cmd.done);
+    },
+
+    prepare: function(cmd) {
+      var text = cmd.text;
+      var values = cmd.values || [];
+      var params = cmd.params || {};
+      text = text.replace(named_param_re, function(match) {
         values.push(params[match.slice(1)]);
         return '$' + values.length;
-      });      
-      return { text: sql, values: values, name: config.name };
-    }  
-  
+      });
+      if (text.slice(0, 7) === 'INSERT ') text += ' RETURNING _id';
+      return { text: text, values: values, name: cmd.name };
+    }
+
   });
 
 });
