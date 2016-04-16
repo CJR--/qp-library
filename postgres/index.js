a018666debc0efbe58be5766317e467b9a320bef
Chris Richardson <cjr--@users.noreply.github.com>
2016-04-16 22:27:15 +0100

    postgres - fix sql prepare

--- postgres/index.js
+++ postgres/index.js
@@ -53,14 +53,14 @@ define(module, (exports, require, make) => {
     },
 
     prepare: function(config) {
-      var type = config.text.slice(0, 6);
+      var text = qp.is(config.text, 'array') ? config.text.join(' ') : config.text;
+      var type = text.slice(0, 6);
       var cmd = {
         name: config.name,
         type: type,
         non_query: qp.inlist(type, 'INSERT', 'UPDATE', 'DELETE')
       };
       cmd[qp.lower(type)] = true;
-      var text = qp.is(config.text, 'array') ? config.text.join(' ') : config.text;
       var values = config.values || [];
       var data = config.data || {};
       cmd.text = text.replace(named_param_re, function(match) {
