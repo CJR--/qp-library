584150c7311db3505a27619d234f6f149183c7cd
Chris Richardson <cjr@hotmail.co.uk>
2016-08-12 00:29:59 +0100

    version - automate package.json version updates

---
+++ version/index.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var semver = require('semver');
+
+module.exports = function(bump, filename, nosave) {
+  filename = filename || 'package.json';
+  filename = path.join(process.cwd(), filename);
+  var package_file = JSON.parse(fs.readFileSync(filename));
+  var version = semver.valid(package_file.version);
+  if (version) {
+    if (bump) {
+      if (nosave == false) {
+        package_file.version = semver.inc(version, bump);
+        fs.writeFileSync(filename, JSON.stringify(package_file, null, '  '));
+      }
+      return { from: version, to: package_file.version, bump: bump };
+    } else {
+      return version;
+    }
+  } else {
+    return '';
+  }
+
+};
