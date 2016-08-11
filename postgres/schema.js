9119c528683343f9787a53a7787262e0a26a9172
Chris Richardson <cjr@hotmail.co.uk>
2016-08-12 00:30:22 +0100

    postgres - txn & schema commands

---
+++ postgres/schema.js
@@ -0,0 +1,99 @@
+define(module, (exports, require, make) => {
+
+  var qp = require('qp-utility');
+
+  make({
+
+    ns: 'qp-library/postgres/schema',
+
+    connection: null,
+
+    create_table: function(data, done) {
+      this.execute({
+        text: [
+          'CREATE TABLE', data.table.name, '(',
+            qp.map(data.columns, column => {
+              var def = column.name;
+              if (column.primary_key) {
+                def += ' SERIAL PRIMARY KEY';
+              } else {
+                def += ' ' + column.type;
+                if (column.is_array) def += '[]';
+              }
+              return def;
+            }).join(', '),
+          ')'
+        ],
+        done: done
+      });
+    },
+
+    drop_table: function(data, done) {
+      this.execute({
+        text: [ 'DROP TABLE IF EXISTS', data.table.name ],
+        done: done
+      });
+    },
+
+    rename_table: function(data, done) {
+      this.execute({
+        text: [ 'ALTER TABLE', data.table.name, 'RENAME TO', data.table.new_name ],
+        done: done
+      });
+    },
+
+    add_column: function() {
+      this.execute({
+        text: [ 'ALTER TABLE', data.table.name, 'ADD COLUMN', data.column.name, data.column.type ],
+        done: done
+      });
+    },
+
+    remove_column: function(data, done) {
+      this.execute({
+        text: [ 'ALTER TABLE', data.table.name, 'DROP COLUMN', data.column.name ],
+        done: done
+      });
+    },
+
+    change_column: function(data, done) {
+      this.execute({
+        text: [ 'ALTER TABLE', data.table.name, 'ALTER COLUMN', data.column.name, 'TYPE', data.column.type ],
+        done: done
+      });
+    },
+
+    rename_column: function(data, done) {
+      this.execute({
+        text: [ 'ALTER TABLE', data.table.name, 'RENAME COLUMN', data.column.name, 'TO', data.column.new_name ],
+        done: done
+      });
+    },
+
+    add_index: function() { },
+    remove_index: function() { },
+    add_constraint: function() { },
+    remove_constraint: function() { },
+
+    execute: function(config) {
+      var done = config.done || qp.noop;
+      var cmd = this.prepare(config);
+      this.connection.query(cmd.pg, (error, pg_result) => {
+        if (error) { done(error); } else {
+          var result = { cmd: cmd };
+          done(null, result);
+        }
+      });
+    },
+
+    prepare: function(config) {
+      var text = qp.is(config.text, 'array') ? config.text.join(' ') : config.text;
+      var cmd = { name: config.name, text: text, type: config.type };
+      cmd[qp.lower(cmd.type)] = true;
+      cmd.pg = { name: cmd.name, text: cmd.text };
+      return cmd;
+    }
+
+  });
+
+});
---
+++ postgres/txn.js
@@ -0,0 +1,33 @@
+define(module, function(exports, require, make) {
+
+  make({
+
+    ns: 'qp-library/postgres/txn',
+
+    connection: null,
+
+    init: function(o) { },
+
+    begin: function(done) {
+      this.connection.query('BEGIN', (error) => {
+        if (error) this.rollback(done); else done();
+      });
+    },
+
+    rollback: function(done) {
+      this.connection.query('ROLLBACK', (error) => {
+        done(error);
+        this.reset();
+      });
+    },
+
+    commit: function(done) {
+      this.connection.query('COMMIT', (error) => {
+        done(error);
+        this.reset();
+      });
+    }
+
+  });
+
+});
