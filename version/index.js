4dae891355c6a69485727eab5459b04f9f4e8243
Chris Richardson <cjr@hotmail.co.uk>
2017-02-24 16:34:44 +0000

    remove nosave option and add 'bump' option

--- version/index.js
+++ version/index.js
@@ -2,18 +2,22 @@ var fs = require('fs');
 var path = require('path');
 var semver = require('semver');
 
-module.exports = function(bump, filename, nosave) {
+module.exports = function(bump, filename) {
   filename = filename || 'package.json';
   filename = path.join(process.cwd(), filename);
   var package_file = JSON.parse(fs.readFileSync(filename));
   var version = semver.valid(package_file.version);
   if (version) {
     if (bump) {
-      if (bump !== 'none') {
+      if (bump === 'major' || bump === 'minor' || bump === 'patch') {
         package_file.version = semver.inc(version, bump);
       }
-      if (!nosave && bump !== 'none') {
+      if (bump === 'major' || bump === 'minor' || bump === 'patch') {
         fs.writeFileSync(filename, JSON.stringify(package_file, null, '  '));
+      } else if (bump === 'bump') {
+        var stat = fs.statSync(filename);
+        var time = (new Date()).getTime();
+        fs.utimesSync(filename, stat.atime, time);
       }
       return { from: version, to: package_file.version, bump: bump };
     } else {
@@ -22,5 +26,4 @@ module.exports = function(bump, filename, nosave) {
   } else {
     return '';
   }
-
 };
