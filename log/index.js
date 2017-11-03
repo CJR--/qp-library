e33736058dff47ac4d95c7b1de5e5a5982bf13cf
Chris Richardson <cjr@hotmail.co.uk>
2017-11-03 16:11:42 +0000

    log; fix color coding in chrome console

--- log/index.js
+++ log/index.js
@@ -5,15 +5,21 @@ define(module, function(exports, require) {
   var fss = require('qp-library/fss');
   var os = require('os');
 
+  var debug = /--inspect/.test(process.execArgv.join(' '));
+
   var log = function log() {
     console.log.apply(console, qp.compact(arguments));
   };
 
   log.clear = function() {
-    if (process.platform === 'win32') {
-      process.stdout.write('\033c');
+    if (debug) {
+      console.clear();
     } else {
-      process.stdout.write('\u001B[2J\u001B[0;0f');
+      if (process.platform === 'win32') {
+        process.stdout.write('\033c');
+      } else {
+        process.stdout.write('\u001B[2J\u001B[0;0f');
+      }
     }
   };
 
@@ -37,17 +43,32 @@ define(module, function(exports, require) {
     console.log(log.yellow(qp.rpad(type, 8)), _1 || '', _2 || '', _3 || '');
   };
 
-  log.blue_white = function(s) { return '\x1b[47m\x1b[34m' + s + '\x1b[0m\x1b[0m'; };
-  log.red_white = function(s) { return '\x1b[47m\x1b[31m' + s + '\x1b[0m\x1b[0m'; };
-  log.white_red = function(s) { return '\x1b[41m\x1b[37m' + s + '\x1b[0m\x1b[0m'; };
-  log.yellow_black = function(s) { return '\x1b[43m\x1b[30m' + s + '\x1b[0m\x1b[0m'; };
-  log.red = function(s) { return '\x1b[31m' + s + '\x1b[0m'; };
-  log.green = function(s) { return '\x1b[32m' + s + '\x1b[0m'; };
-  log.yellow = function(s) { return '\x1b[33m' + s + '\x1b[0m'; };
-  log.blue = function(s) { return '\x1b[34m' + s + '\x1b[0m'; };
-  log.magenta = function(s) { return '\x1b[35m' + s + '\x1b[0m'; };
-  log.cyan = function(s) { return '\x1b[36m' + s + '\x1b[0m'; };
-  log.white = function(s) { return '\x1b[37m' + s + '\x1b[0m'; };
+  if (debug) {
+    var plain_text = function(s) { return (typeof s === 'string' ? s.trim() : '' + s); };
+    log.blue_white = plain_text;
+    log.red_white = plain_text;
+    log.white_red = plain_text;
+    log.yellow_black = plain_text;
+    log.red = plain_text;
+    log.green = plain_text;
+    log.yellow = plain_text;
+    log.blue = plain_text;
+    log.magenta = plain_text;
+    log.cyan = plain_text;
+    log.white = plain_text;
+  } else {
+    log.blue_white = function(s) { return '\x1b[47m\x1b[34m' + s + '\x1b[0m\x1b[0m'; };
+    log.red_white = function(s) { return '\x1b[47m\x1b[31m' + s + '\x1b[0m\x1b[0m'; };
+    log.white_red = function(s) { return '\x1b[41m\x1b[37m' + s + '\x1b[0m\x1b[0m'; };
+    log.yellow_black = function(s) { return '\x1b[43m\x1b[30m' + s + '\x1b[0m\x1b[0m'; };
+    log.red = function(s) { return '\x1b[31m' + s + '\x1b[0m'; };
+    log.green = function(s) { return '\x1b[32m' + s + '\x1b[0m'; };
+    log.yellow = function(s) { return '\x1b[33m' + s + '\x1b[0m'; };
+    log.blue = function(s) { return '\x1b[34m' + s + '\x1b[0m'; };
+    log.magenta = function(s) { return '\x1b[35m' + s + '\x1b[0m'; };
+    log.cyan = function(s) { return '\x1b[36m' + s + '\x1b[0m'; };
+    log.white = function(s) { return '\x1b[37m' + s + '\x1b[0m'; };
+  }
 
   exports('qp-library/log', log);
 
